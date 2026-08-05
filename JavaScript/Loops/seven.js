28bc99577fd772b03af2aecc47a83042e66609da
Uzair <uzairkhan03010@gmail.com>
2026-08-05 12:11:49 +0500

    for of

---
+++ JavaScript/Loops/seven.js
@@ -0,0 +1,21 @@
+///For of loop
+
+let cities =  [
+
+    { name: "New York", population: 8175133},
+    { name: "Cairo"  ,  population: 18.890e3},
+    { name: "Tokyo"  ,  population: 21.515e6},
+    { name: "London" ,  population: 20.673e6},
+    { name: "Paris"  ,  population: 2.206e6},
+    { name: "Berlin" ,  population: 22.645e6},
+    { name: "Madrid" ,  population: 3.223e6},
+
+];
+
+
+for (let city of cities) { 
+    if ( city.population > 20e6) {
+        console.log(`${city.name} (${city.population})`);
+    }
+
+}
