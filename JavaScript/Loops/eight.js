31ffaf5f6eec7f4d8e69707754dbc24d2c537896
Uzair <uzairkhan03010@gmail.com>
2026-08-05 12:14:02 +0500

    for in

---
+++ JavaScript/Loops/eight.js
@@ -0,0 +1,15 @@
+//for in 
+
+let user = {
+    name: "wick",
+    sirname: "Hart",
+    age:34,
+    email: "wickHart@tele.us"
+
+}
+
+for (let key in user) {
+    console.log(key);
+};
+
+console.log(user.email)
