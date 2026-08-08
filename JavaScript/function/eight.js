aee80d607db2daeb821387a79f1ca43a0f6ebcbe
Uzair <uzairkhan03010@gmail.com>
2026-08-08 23:45:27 +0500

    ...

---
+++ JavaScript/function/eight.js
@@ -0,0 +1,15 @@
+let a = 100, b = 200,  c = 400;
+
+function test(a) {
+    let b = 10;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+}
+
+test(1);
+
+console.log(a);
+console.log(b);
+console.log(c);
