cafc94e21437378f9633f9cff03b413dd9c2d930
Uzair <uzairkhan03010@gmail.com>
2026-08-08 23:44:32 +0500

    ...

---
+++ JavaScript/function/seven.js
@@ -0,0 +1,10 @@
+function add(first, second) {
+    return first + second;
+
+}
+
+let first = 10 , second = 20 , third = 40, fourth = 80;
+console.log(add(first,second));
+console.log(add(second,third));
+console.log(add(third,fourth))
+
